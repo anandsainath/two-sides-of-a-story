f01e24beef0a849787c2620799c9a596cea43578
Anand Sainath <anand.sainath@gmail.com>
2014-10-13 04:10:19 -0400

    Random API key for getting the share count.

--- app/data/shares.py
+++ app/data/shares.py
@@ -2,12 +2,14 @@ import json
 import urllib
 import sys
 import utils
+import random
 
 def get_social_counts(urlparam="http://www.google.com"):
 
-	API_KEY = "8ca7bc67af37fe0affd10a53c818e431b8d8bfba"
+	API_KEY = ["8ca7bc67af37fe0affd10a53c818e431b8d8bfba", "115a154ac6fe21c051c0346cbebf42d9a5a00c8d"]
 	domain = "http://free.sharedcount.com/"
-	params = { 'url' : urlparam, 'apikey' : API_KEY}
+	params = { 'url' : urlparam, 'apikey' : random.choice(API_KEY)}
 
+	print params
 	url = domain + "?"+urllib.urlencode(params)
 	return utils.get_json_response(url)
